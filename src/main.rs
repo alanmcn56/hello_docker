a92f4e1b041e93a5ffa92e251d945dd291921ef8
Alan McNamee <alanmcn@gmail.com>
2026-07-16 22:41:20 +0100

    Wow message

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     let target = "Docker Container";
-    println!("Hello, {}! Your Rust app is running successfully.", target);
+    println!("Hello, {}! Your Rust app is running successfully. Wow!", target);
 }
